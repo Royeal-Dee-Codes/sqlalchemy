0a5da327d0f553f309652cc418144db19c07743d
highqualidee <highqualidee@gmail.com>
2024-06-20 22:32:06 -0600

    Updates products controller

--- controllers/products_controller.py
+++ controllers/products_controller.py
@@ -71,43 +71,71 @@ def product_get_by_id(product_id):
     return jsonify({"message": "product found", "results": product_schema.dump(product_query)}), 200
 
 
-def product_category_add(request):
-    post_data = request.form if request.form else request.json
+def product_category_add(req, product_id):
+    post_data = req.form if req.form else req.json()
 
-    product_id = post_data.get('product_id')
-    category_id = post_data.get('category_id')
+    # product_id = post_data.get('product_id')
+    # category_id = post_data.get('category_id')
+
+    # product_query = db.session.query(Products).filter(Products.product_id == product_id).first()
+    # category_query = db.session.query(Categories).filter(Categories.category_id == category_id).first()
+    new_product = Products.new_product_obj()
+    populate_object(new_product, post_data)
+
+    try:
+        db.session.add(new_product)
+        db.session.commit()
+    except:
+        db.session.rollback()
+        return jsonify({"message": "unable to update record"}), 400
+
+    return jsonify({"message": "product created", "result": products_schema.dump(new_product)}), 201
+
+
+def products_get_all():
+    products_query = db.session.query(Products).all()
+
+    print(products_query)
+
+    return jsonify({"message": "products found", "results": products_schema.dump(products_query)}), 200
+
+
+def product_get_by_id(product_id):
+    product_query = db.session.query(Products).filter(Products.product_id == product_id).first()
+
+    if not product_query:
+        return jsonify({"message": f"product does not exist"}), 400
+
+    return jsonify({"message": "product found", "result": product_schema.dump(product_query)}), 200
+
+
+def product_update(req, product_id):
+    post_data = req.form if req.form else req.json
 
     product_query = db.session.query(Products).filter(Products.product_id == product_id).first()
-    category_query = db.session.query(Categories).filter(Categories.category_id == category_id).first()
 
-    if product_query:
-        if category_query:
-            product_query.categories.append(category_query)
+    populate_object(product_query, post_data)
 
-            db.session.commit()
+    try:
+        db.session.commit()
+    except:
+        db.session.rollback()
+        return jsonify({"message": "unable to update record"}), 400
 
-        categories_list = []
+    return jsonify({"message": "product updated", "result": product_schema.dump(product_query)}), 200
 
-        for category in product_query.categories:
-            categories_list.append({
-                'category_id': category.category_id,
-                'category_name': category.category_name
-            })
 
-        company_dict = {
-            'company_id': product_query.company.company_id,
-            'company_name': product_query.company.company_name
-        }
+def product_delete(product_id):
+    product_query = db.session.query(Products).filter(Products.product_id).first()
 
-        product_dict = {
-            'product_id': product_query.product_id,
-            'product_name': product_query.product_name,
-            'description': product_query.description,
-            'price': product_query.price,
-            'active': product_query.active,
-            'company': company_dict,
-            'categories': categories_list
-            # 'warranty': warranty_dict,
-        }
+    if not product_query:
+        return jsonify({"message": f"product {product_id} does not exist"}), 404
 
-    return jsonify({"message": "product added to category", "results": product_dict}), 200
+    try:
+        db.session.delete(product_query)
+        db.session.commit()
+    except:
+        db.session.rollback()
+        return jsonify({"message": "unable to delete"}), 400
+
+    return jsonify({"message": "product delelted", "result": product_schema.dump(product_query)}), 200
