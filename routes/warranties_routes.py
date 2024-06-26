9a9ddd3bcfebdba9e5e1b260d6fe585a02cc0281
highqualidee <highqualidee@gmail.com>
2024-06-25 20:23:02 -0600

    updates warrities routes and blueprints

--- routes/warranties_routes.py
+++ routes/warranties_routes.py
@@ -2,24 +2,24 @@ from flask import request, Blueprint
 
 import controllers
 
-warranty = Blueprint('warranty', __name__)
+warranties = Blueprint('warranty', __name__)
 
 
-@warranty.route('/warranty', methods=['POST'])
+@warranties.route('/warranty', methods=['POST'])
 def warranty_add():
     return controllers.warranty_add(request)
 
 
-@warranty.route('/warranty/<warranty_id>', methods=['GET'])
+@warranties.route('/warranty/<warranty_id>', methods=['GET'])
 def warranty_get_by_id(warranty_id):
     return controllers.warranty_get_by_id(warranty_id)
 
 
-@warranty.route('/warranties', methods=['GET'])
+@warranties.route('/warranties', methods=['GET'])
 def warranties_get_all():
     return controllers.warranties_get_all(request)
 
 
-@warranty.route('/warranty/<warranty_id>', methods=['PUT'])
+@warranties.route('/warranty/<warranty_id>', methods=['PUT'])
 def warranty_update(warranty_id):
     return controllers.warranty_update(request, warranty_id)
--- util/blueprints.py
+++ util/blueprints.py
@@ -5,6 +5,6 @@ def register_blueprints(app):
     app.register_blueprint(routes.products)
     app.register_blueprint(routes.categories)
     app.register_blueprint(routes.companies)
-    app.register_blueprint(routes.warranty)
+    app.register_blueprint(routes.warranties)
     app.register_blueprint(routes.auth)
     app.register_blueprint(routes.users)
